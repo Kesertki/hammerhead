fe93a23d4903ba2ff76622c8b9bdf76be1294e0d
Denys Vuika <denys.vuika@gmail.com>
2025-08-07 15:15:37 -0400

    refactor: simplify welcome component by removing unused imports and button for model file selection

--- src/App/components/Welcome/Welcome.tsx
+++ src/App/components/Welcome/Welcome.tsx
@@ -1,29 +1,15 @@
-import { HardDriveUpload } from 'lucide-react';
-import { useCallback } from 'react';
-import { Button } from '@/components/ui/button.tsx';
-import { electronLlmRpc } from '@/rpc/llmRpc.ts';
 import headerImage from '/header.webp';
 
 import './Welcome.css';
 
 export function Welcome() {
-    const openSelectModelFileDialog = useCallback(async () => {
-        await electronLlmRpc.selectModelFileAndLoad(true);
-    }, []);
-
     return (
         <div className="welcome-container">
             <h1 className="welcome">
                 Welcome to Hammerhead<sup className="alpha">(alpha)</sup>
             </h1>
             <div className="hint">
-                <p className="hint-text">
-                    <Button variant="link" size="sm" className="cursor-pointer" onClick={openSelectModelFileDialog}>
-                        <HardDriveUpload className="inline-icon mr-1" />
-                        Choose a model file
-                    </Button>{' '}
-                    to start chatting
-                </p>
+                <p className="hint-text">Choose a model file to start chatting</p>
             </div>
             <img src={headerImage} alt="Hammerhead Logo" />
         </div>
